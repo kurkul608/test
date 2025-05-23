e0c7df2db0788398d413062b9d7b0448d4821c06
Petr Molchanov <molc.pyotr@yandex.ru>
2025-05-23 08:09:36 -0300

    upd5

--- src/ChatBugDemo.tsx
+++ src/ChatBugDemo.tsx
@@ -1,19 +1,6 @@
-import { useEffect } from 'react';
 import './ChatBugDemo.css';
 
 export default function ChatBugDemo() {
-    useEffect(() => {
-        // Этот хак нужен, чтобы при изменении высоты окна (открытие клавиатуры) обновлялось значение vh
-        const setVh = () => {
-            const vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty('--vh', `${vh}px`);
-        };
-
-        setVh();
-        window.addEventListener('resize', setVh);
-
-        return () => window.removeEventListener('resize', setVh);
-    }, []);
 
     return (
         <div className="chat">

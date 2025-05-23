579a544d39e659bd8096e23923d40ebe110a6890
Petr Molchanov <molc.pyotr@yandex.ru>
2025-05-23 08:12:10 -0300

    upd7

--- src/ChatBugDemo.tsx
+++ src/ChatBugDemo.tsx
@@ -4,20 +4,17 @@ export default function ChatBugDemo() {
 
     return (
         <div className="chat">
-            <div className="chat-wrap">
-                <div className="chat-messages">
-                    <div className="message">Привет! Это тест на баг с клавиатурой.</div>
-                    <div className="message">Попробуй нажать на инпут в Telegram Mini App 😉</div>
-                </div>
-                <form className="chat-input-form">
+            <div className="chat-messages">
+                <div className="message">Привет! Это тест на баг с клавиатурой.</div>
+                <div className="message">Попробуй нажать на инпут в Telegram Mini App 😉</div>
+            </div>
+            <form className="chat-input-form">
         <textarea
             placeholder="Напиши сообщение..."
             className="chat-textarea"
             rows={1}
         />
-                </form>
-            </div>
-
+            </form>
         </div>
     );
 }
